aebde3516eaadc7678bfda488c1d50446c879a15
minagishl <mvxd568.co@icloud.com>
2025-09-02 18:01:53 +0900

    build: add sitemap plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import tailwindcss from '@tailwindcss/vite';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact(), tailwindcss()],
+  plugins: [
+    preact(),
+    tailwindcss(),
+    Sitemap({
+      hostname: 'https://drowse.minagishl.com',
+    }),
+  ],
 });
