375782273ea8f4dc24c337c3c03fbdc0dade3f23
minagishl <mvxd568.co@icloud.com>
2025-09-02 18:06:45 +0900

    build: add minify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import tailwindcss from '@tailwindcss/vite';
 import Sitemap from 'vite-plugin-sitemap';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,14 @@ export default defineConfig({
     tailwindcss(),
     Sitemap({
       hostname: 'https://drowse.minagishl.com',
+    }),
+    ViteMinifyPlugin({
+      removeComments: true,
+      collapseWhitespace: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true,
+      useShortDoctype: true,
+      removeEmptyAttributes: false,
     }),
   ],
 });
